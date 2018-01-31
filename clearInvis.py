c4508169e610176a8c976cf959e109b7890d856b
minh <nguyenhminhp@gmail.com>
2018-01-30 17:07:22 -0800

    final

---
+++ clearInvis.py
@@ -0,0 +1,4 @@
+with open("server.py") as fp:
+    for i, line in enumerate(fp):
+        if "\xe2" in line:
+            print i, repr(line)
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from myconnection import MySQLConnector
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 app.secret_key = "helrakjdl;kfsadfs"
 mysql = MySQLConnector(app, "airbnbdb")
@@ -16,5 +16,23 @@ def index():
 	# 	print user["password"]
 	# 	print user["created_at"]
 	# 	print user["updated_at"]
+
+
+
+@app.route("/add", methods=["POST"])
+def add():
+	query = "INSERT INTO users (`email`, `password`, `created_at`, `updated_at`) VALUES (:spot_one, :spot_two, now(), now());"
+	data = {
+		'spot_one':  request.form['email'],   
+		'spot_two':  request.form['password'],
+	}
+	mysql.query_db(query, data)
+
+# //querry to insetd da
+	return redirect("/")
+
+@app.route("/create")
+def create():
+	return render_template("create.html")
 
 app.run(debug=True)
